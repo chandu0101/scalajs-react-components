8f7f8bf7f96bde9a22c9a95889f838b2dcf2d101
TP Diffenbach <tpd@diffenbach.org>
2015-09-21 09:23:33 -0700

    Make date format selection a dropdown.

--- demo/src/main/scala/demo/components/materialui/MuiUpdatingComponentsDemo.scala
+++ demo/src/main/scala/demo/components/materialui/MuiUpdatingComponentsDemo.scala
@@ -2,13 +2,12 @@ package demo
 package components
 package materialui
 
-import chandu0101.scalajs.react.components.fascades.LatLng
 import chandu0101.scalajs.react.components.materialui._
-import japgolly.scalajs.react.ReactComponentB
-import japgolly.scalajs.react._, vdom.prefix_<^._, ScalazReact._, MonocleReact._
-import scala.scalajs.js.{UndefOr, Any, Date}
-import japgolly.scalajs.react.extra.ExternalVar
-import monocle.macros._
+import japgolly.scalajs.react.{ReactComponentB, _}
+import japgolly.scalajs.react.vdom.prefix_<^._
+
+import scala.scalajs.js
+import scala.scalajs.js.Date
 import scala.scalajs.js.Dynamic.global
 
 
@@ -16,26 +15,34 @@ import scala.scalajs.js.Dynamic.global
 object MuiUpdatingComponentsDemo {
   
   //@Lenses
-  case class DateSpan(start: Date, end: Date, errorMsg: Option[String],
-                      pickerMode: MuiDatePickerMode, useAutoOk: Boolean, useYYYYMMDD: Boolean)
+  case class DateSpan(start: Date, end: Date, errorMsg: Option[String], useAutoOk: Boolean,
+                      pickerMode: MuiDatePickerMode, dateFormatterOffset: Int)
 
+  def ??[T](a : T, b: => T) : T = if (a != null) a else b
   def after(a: Date, b: Date) = a != null && b != null && a.getTime > b.getTime
-  def dateFormatterYYYMMDD = (d: Date) => if (d == null) "" else f"${d.getFullYear}-${d.getMonth + 1}%02d-${d.getDate}%02d"
-  def dateFormatterToString = (d: Date) => if (d == null) "" else d.toString
   def onlyWeekdays = (d: Date) => (d.getDay() + 1) % 6 != 1
 
-  def modeName(b: Boolean) = if (b) MuiDatePickerMode.LANDSCAPE else MuiDatePickerMode.PORTRAIT
   def autoOkName(b: Boolean) = if (b) "on" else "off"
-  def dateFormatName(b: Boolean) = if (b) "YYYY-MM-DD" else "String"
+
+  val dateFormatters = Seq[(String, Date => String)](
+    "YYYY-MM-DD"     -> ((d: Date) => if (d == null) "" else f"${d.getFullYear}-${d.getMonth + 1}%02d-${d.getDate}%02d"),
+    "toString"       -> ((d: Date) => if (d == null) "" else d.toString),
+    "US MM/DD/YYYY"  -> ((d: Date) => if (d == null) "" else f"${d.getMonth + 1}%02d/${d.getDate}%02d/${d.getFullYear}"), // US
+    "EUR DD/MM/YYYY" -> ((d: Date) => if (d == null) "" else f"${d.getDate}%02d/${d.getMonth + 1}%02d/${d.getFullYear}") // Eur
+  )
+
+  val dateFormattersMenuItems = dateFormatters.zipWithIndex.map {
+    case((label, f), i) => MuiDropdownMenuItem(payload = i.toString, text = label + " " + f(new Date()))
+  }.toJsArray
 
   val code =
     """
-      | MuiDatePicker(hintText = "Protrait Dialog"),
-      | MuiDatePicker(hintText = "Landscape Dialog",mode = MuiDatePickerMode.LANDSCAPE)
+      | Get code from a macro.
       |
     """.stripMargin
+
   val component = ReactComponentB[Unit]("MuiUpdatingComponentsDemo")
-    .initialState(DateSpan(null, null, None, MuiDatePickerMode.PORTRAIT, false, false))
+    .initialState(DateSpan(null, null, None, false, MuiDatePickerMode.PORTRAIT, 0))
     .render( $ => {
 
 
@@ -44,12 +51,10 @@ object MuiUpdatingComponentsDemo {
       def updateEnd         = (_: Date, d: Date) => $.modState(_.copy(end = d))
       def modeSwitch        = (_: ReactEvent, mode: String) => $.modState(_.copy(pickerMode = MuiDatePickerMode.newMode(mode)))
       def autoOkToggle      = (_: ReactEvent, toggled: Boolean) => $.modState(_.copy(useAutoOk = toggled))
-      def dateFormatToggle  = (_: ReactEvent, toggled: Boolean) => $.modState(_.copy(useYYYYMMDD = toggled))
+      def dateFormatUpdate  = (_: ReactEvent, index: Int, _: js.Object) => $.modState(_.copy(dateFormatterOffset = index))
+
 
       def onSubmit : ReactEventH => Unit = (_:ReactEventH) => global.alert(" Do something Ajax-y with: " + $.state)
-
-      def ??[T](a : T, b: => T) : T = if(a != null) a else b
-
 
       val state = $.state
 
@@ -59,6 +64,13 @@ object MuiUpdatingComponentsDemo {
         CodeExample(code,"MuiUpdatingComponentsDemo")(
           MuiPaper(zDepth = 3)(
             <.div(^.padding := 3.em,
+
+              MuiToggle(
+                onToggle = autoOkToggle,
+                defaultToggled = state.useAutoOk,
+                labelPosition = MuiSwitchLabelPosition.RIGHT,
+                label="Switch to AutoOk:" + autoOkName(! state.useAutoOk) + " DatePickers"
+              )(),
 
               // while we can use a toggle for the picker orientation,
               // a radio group is more appropriate, because no mode
@@ -73,63 +85,62 @@ object MuiUpdatingComponentsDemo {
                     MuiRadioButton(value = MuiDatePickerMode.LANDSCAPE.value, label = MuiDatePickerMode.LANDSCAPE.value)()
                   )
               ),
-              MuiToggle(
-                onToggle = autoOkToggle,
-                defaultToggled = state.useAutoOk,
-                labelPosition = MuiSwitchLabelPosition.RIGHT,
-                label="Switch to AutoOk:" + autoOkName(! state.useAutoOk) + " DatePickers"
-              )(),
 
-              MuiToggle(
-                onToggle = dateFormatToggle,
-                defaultToggled = state.useYYYYMMDD,
-                labelPosition = MuiSwitchLabelPosition.RIGHT,
-                label="Switch to " + dateFormatName(! state.useYYYYMMDD) + " date format DatePickers"
-              )(),
-               <.h2("Select your travel dates"),
-               //<.div(
-                 <.label("Depart On:",
-               MuiDatePicker(
-                 defaultDate = state.start,
-                 hintText = "Start Date",
-                 onChange = updateStart,
-                 minDate = new Date(),
-                 mode = state.pickerMode,
-                 autoOk = state.useAutoOk,
-                 formatDate = if (state.useYYYYMMDD) dateFormatterYYYMMDD else dateFormatterToString
-               )()),
+              <.label("Date display:",
+                MuiDropdownMenu(
+                  menuItems = dateFormattersMenuItems,
+                  onChange = dateFormatUpdate)()
+              ),
+
+              <.h2("Select your travel dates"),
+
+              <.label("Depart On:",
+                MuiDatePicker(
+                   defaultDate = state.start,
+                   hintText = "Date Departing",
+                   onChange = updateStart,
+                   minDate = new Date(),
+                   mode = state.pickerMode,
+                   autoOk = state.useAutoOk,
+                   formatDate = dateFormatters(state.dateFormatterOffset)._2
+                 )()
+              ),
+
               <.label("Return On:",
-               MuiDatePicker(
-                 defaultDate = state.end,
-                 hintText = "End Date",
-                 onChange = updateEnd,
-                 minDate = ??(state.start, new Date()),
-                 mode = state.pickerMode,
-                 autoOk = state.useAutoOk,
-                 formatDate = if (state.useYYYYMMDD) dateFormatterYYYMMDD else dateFormatterToString
-               )()),
-                             //shouldDisableDate = onlyWeekdays)(),
+                 MuiDatePicker(
+                   defaultDate = state.end,
+                   hintText = "Date Returning",
+                   onChange = updateEnd,
+                   minDate = ??(state.start, new Date()),
+                   mode = state.pickerMode,
+                   autoOk = state.useAutoOk,
+                   formatDate = dateFormatters(state.dateFormatterOffset)._2
+                 )()
+              ),
 
-               MuiRaisedButton(
-                 primary = true,
-                 label = "Find Reservations",
-                 disabled = state.end == null,
-                 onTouchTap = onSubmit
-               )(),
-              <.p(
-                """
-                  | While we can use a toggle for the picker orientation,
-                  | a radio group is more appropriate, because no mode
-                  | maps to "off" and the other to "on".
-                  | We will also change DateSpan's Boolean (is)landscapeMode to a String, pickerMode.
-                """.stripMargin)
+              MuiRaisedButton(
+                   primary = true,
+                   label = "Find Reservations",
+                   disabled = state.end == null,
+                   onTouchTap = onSubmit
+                 )(),
+              <.p("""
+                    | Note: date format changes only occur after a date is selected.
+                  """.stripMargin
+              ),
+              <.p("""
+                    | While we can use a toggle for the picker orientation,
+                    | a radio group is more appropriate, because no mode
+                    | maps to "off" and the other to "on".
+                    | We will also change DateSpan's Boolean (is)landscapeMode to a String, pickerMode.
+                    | There's a small problem when the toggle is under the dropdow:
+                    | clicking a dropdown sometimes causing the toggle on click to occur,
+                    | and the dropdown onclick is apparently ignored?
+                  """.stripMargin
+              )
             )
           ),
-
-           <.div(dateFormatterToString(state.start)),
-           <.div(if (state.end == null) "Not Set" else state.end.toString),
-           <.div(state.errorMsg.map( s => <.span(s))),
-           <.button(^.onClick --> {global.alert(" Do something Ajax-y with:" + state)}, "Submit")
+          <.div(state.errorMsg.map( s => <.span(s)))
         )
       )
   }).buildU

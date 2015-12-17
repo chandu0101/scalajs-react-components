0dd703cbf14595bd8c093db02614316bdd89c9a9
Roberto Leibman <roberto@leibman.net>
2015-12-17 14:56:13 -0800

    Forgot to add the demo file

--- demo/src/main/scala/demo/routes/ReactTableRouteModule.scala
+++ demo/src/main/scala/demo/routes/ReactTableRouteModule.scala
@@ -7,25 +7,24 @@ import japgolly.scalajs.react.extra.router.RouterConfigDsl
 
 object ReactTableRouteModule {
 
-  case object Info extends LeftRoute("Info", "info", () => ReactTableInfo())
+  case object Info extends LeftRoute("Info", "info", () ⇒ ReactTableInfo())
 
-  case object Basic extends LeftRoute("Search ,Pagination", "basic", () => ReactTableBasic())
+  case object Basic extends LeftRoute("Search ,Pagination", "basic", () ⇒ ReactTableBasic())
 
-  case object Sorting extends LeftRoute("Sorting", "sort", () => ReactTableSorting())
+  case object Sorting extends LeftRoute("Sorting", "sort", () ⇒ ReactTableSorting())
 
-  case object CustomCell extends LeftRoute("Custom Cell", "customcell", () => ReactTableCustomCell())
+  case object CustomCell extends LeftRoute("Custom Cell", "customcell", () ⇒ ReactTableCustomCell())
 
-  case object CustomCellWithButtons extends LeftRoute("Cell With Buttons", "customcellwithbuttons", () => ReactTableCustomCellWithButtons())
+  case object CustomCellWithButtons extends LeftRoute("Cell With Buttons", "customcellwithbuttons", () ⇒ ReactTableCustomCellWithButtons())
 
-  case object CustomColumn extends LeftRoute("Custom Colum Size", "customcolumsize", () => ReactTableCustomColumnSize())
+  case object CustomColumn extends LeftRoute("Custom Colum Size", "customcolumsize", () ⇒ ReactTableCustomColumnSize())
 
-  val menu: List[LeftRoute] = List(Info, Basic, Sorting, CustomCell, CustomColumn)
+  val menu: List[LeftRoute] = List(Info, Basic, Sorting, CustomCell, CustomCellWithButtons, CustomColumn)
 
-  val routes = RouterConfigDsl[LeftRoute].buildRule { dsl =>
+  val routes = RouterConfigDsl[LeftRoute].buildRule { dsl ⇒
     import dsl._
-    menu.map(i =>
-      staticRoute(i.route, i) ~> renderR(r => ReactTablePage(i, r))
-    ).reduce(_ | _)
+    menu.map(i ⇒
+      staticRoute(i.route, i) ~> renderR(r ⇒ ReactTablePage(i, r))).reduce(_ | _)
 
   }
 }

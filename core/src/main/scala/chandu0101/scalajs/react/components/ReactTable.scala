88ec293be90eadd90ab8d1d72281d5b3130e8686
Roberto Leibman <roberto@leibman.net>
2017-08-24 12:31:10 -0700

    I think this is cleaner than the extra components that were being declared and created in a very non-canonical way

--- core/src/main/scala/chandu0101/scalajs/react/components/ReactTable.scala
+++ core/src/main/scala/chandu0101/scalajs/react/components/ReactTable.scala
@@ -9,18 +9,15 @@ import scala.collection.immutable
 import scalacss.ProdDefaults._
 
 import scalacss.ScalaCssReact.scalacssStyleaToTagMod
-import chandu0101.scalajs.react.components.ReactSearchBox
-import chandu0101.scalajs.react.components.Pager
-import chandu0101.scalajs.react.components.DefaultSelect
 
 /**
-  * Companion object of ReactTable, with tons of little utilities
-  */
+ * Companion object of ReactTable, with tons of little utilities
+ */
 object ReactTable {
 
   /**
-    * The direction of the sort
-    */
+   * The direction of the sort
+   */
   object SortDirection extends Enumeration {
     type SortDirection = Value
     val asc, dsc = Value
@@ -28,25 +25,24 @@ object ReactTable {
   /*
    * Pass this to the ColumnConfig to sort using an ordering
    */
-//  def Sort[T, B](fn: T => B)(implicit ordering: Ordering[B]): (T, T) => Boolean = {
-//    (m1: T, m2: T) =>
-//      ordering.compare(fn(m1), fn(m2)) > 0
-//  }
-//  /*
-//   * Pass this to the ColumnConfig to sort a string ignoring case using an ordering
-//   */
-//  def IgnoreCaseStringSort[T](fn: T => String): (T, T) => Boolean =
-//    (m1: T, m2: T) => fn(m1).compareToIgnoreCase(fn(m2)) > 0
+  //  def Sort[T, B](fn: T => B)(implicit ordering: Ordering[B]): (T, T) => Boolean = {
+  //    (m1: T, m2: T) =>
+  //      ordering.compare(fn(m1), fn(m2)) > 0
+  //  }
+  //  /*
+  //   * Pass this to the ColumnConfig to sort a string ignoring case using an ordering
+  //   */
+  //  def IgnoreCaseStringSort[T](fn: T => String): (T, T) => Boolean =
+  //    (m1: T, m2: T) => fn(m1).compareToIgnoreCase(fn(m2)) > 0
 
   def DefaultOrdering[T, B](fn: T => B)(implicit ordering: Ordering[B]) = new Ordering[T] {
-    def compare(a:T, b:T) = ordering.compare(fn(a), fn(b))
+    def compare(a: T, b: T) = ordering.compare(fn(a), fn(b))
   }
-  
+
   def ignoreCaseStringOrdering[T](fn: T => String) = new Ordering[T] {
-    def compare(a:T, b:T) = fn(a).compareToIgnoreCase(fn(b))
+    def compare(a: T, b: T) = fn(a).compareToIgnoreCase(fn(b))
   }
-  
-  
+
   class Style extends StyleSheet.Inline {
 
     import dsl._
@@ -57,12 +53,11 @@ object ReactTable {
       display.flex,
       flexDirection.column,
       boxShadow := "0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24)",
-      media.maxWidth(740 px)(boxShadow := "none")
-    )
+      media.maxWidth(740 px)(boxShadow := "none"))
 
     val tableRow = style(padding :=! "0.8rem",
-                         &.hover(backgroundColor :=! "rgba(244, 244, 244, 0.77)"),
-                         media.maxWidth(740 px)(boxShadow := "0 1px 3px grey", margin(5 px)))
+      &.hover(backgroundColor :=! "rgba(244, 244, 244, 0.77)"),
+      media.maxWidth(740 px)(boxShadow := "0 1px 3px grey", margin(5 px)))
 
     val tableHeader = style(fontWeight.bold, borderBottom :=! "1px solid #e0e0e0", tableRow)
 
@@ -92,15 +87,15 @@ object ReactTable {
     t => fn(t).fold(defaultValue)(bRenderer)
 
   case class ColumnConfig[T](name: String,
-                             cellRenderer: CellRenderer[T],
-                             //sortBy: Option[(T, T) => Boolean] = None,
-                             width: Option[String] = None,
-                             nowrap: Boolean = false)(implicit val ordering: Ordering[T])
+    cellRenderer: CellRenderer[T],
+    //sortBy: Option[(T, T) => Boolean] = None,
+    width: Option[String] = None,
+    nowrap: Boolean = false)(implicit val ordering: Ordering[T])
 
   def SimpleStringConfig[T](name: String,
-                            stringRetriever: T => String,
-                            width: Option[String] = None,
-                            nowrap: Boolean = false): ReactTable.ColumnConfig[T] = {
+    stringRetriever: T => String,
+    width: Option[String] = None,
+    nowrap: Boolean = false): ReactTable.ColumnConfig[T] = {
     val renderer: CellRenderer[T] = if (nowrap) { t =>
       <.span(stringRetriever(t))
     } else { t =>
@@ -111,36 +106,36 @@ object ReactTable {
 }
 
 /**
-  * A relatively simple html/react table with a pager.
-  * You should pass in the data as a sequence of items of type T
-  * But you should also pass a list of Column Configurations, each of which describes how to get to each column for a given item in the data, how to display it, how to sort it, etc.
-  */
+ * A relatively simple html/react table with a pager.
+ * You should pass in the data as a sequence of items of type T
+ * But you should also pass a list of Column Configurations, each of which describes how to get to each column for a given item in the data, how to display it, how to sort it, etc.
+ */
 case class ReactTable[T](data: Seq[T],
-                         configs: List[ReactTable.ColumnConfig[T]] = List(),
-                         rowsPerPage: Int = 5,
-                         style: ReactTable.Style = ReactTable.DefaultStyle,
-                         enableSearch: Boolean = true,
-                         searchBoxStyle: ReactSearchBox.Style = ReactSearchBox.DefaultStyle,
-                         onRowClick: (Int) => Callback = { _ =>
-                           Callback {}
-                         },
-                         searchStringRetriever: T => String = { t: T =>
-                           t.toString
-                         }) {
+    configs: List[ReactTable.ColumnConfig[T]] = List(),
+    rowsPerPage: Int = 5,
+    style: ReactTable.Style = ReactTable.DefaultStyle,
+    enableSearch: Boolean = true,
+    searchBoxStyle: ReactSearchBox.Style = ReactSearchBox.DefaultStyle,
+    onRowClick: (Int) => Callback = { _ =>
+      Callback {}
+    },
+    searchStringRetriever: T => String = { t: T =>
+      t.toString
+    }) {
 
   import ReactTable._
   import SortDirection._
 
   case class State(filterText: String,
-                   offset: Int,
-                   rowsPerPage: Int,
-                   filteredData: Seq[T],
-                   sortedState: Map[Int, SortDirection])
+    offset: Int,
+    rowsPerPage: Int,
+    filteredData: Seq[T],
+    sortedState: Map[Int, SortDirection])
 
   class Backend(t: BackendScope[Props, State]) {
 
-    def onTextChange(P: Props)(value: String): Callback =
-      t.modState(_.copy(filteredData = getFilteredData(value, P.data), offset = 0))
+    def onTextChange(props: Props)(value: String): Callback =
+      t.modState(_.copy(filteredData = getFilteredData(value, props.data), offset = 0))
 
     def onPreviousClick: Callback =
       t.modState(s => s.copy(offset = s.offset - s.rowsPerPage))
@@ -157,79 +152,59 @@ case class ReactTable[T](data: Seq[T],
     }
 
     def sort(ordering: Ordering[T], columnIndex: Int): Callback =
-      t.modState { S =>
-        val rows = S.filteredData
-        S.sortedState.get(columnIndex) match {
+      t.modState { state =>
+        val rows = state.filteredData
+        state.sortedState.get(columnIndex) match {
           case Some(asc) =>
-            S.copy(filteredData = rows.sorted(ordering.reverse),
-                   sortedState = Map(columnIndex -> dsc),
-                   offset = 0)
+            state.copy(filteredData = rows.sorted(ordering.reverse),
+              sortedState = Map(columnIndex -> dsc),
+              offset = 0)
           case _ =>
-            S.copy(filteredData = rows.sorted(ordering),
-                   sortedState = Map(columnIndex -> asc),
-                   offset = 0)
+            state.copy(filteredData = rows.sorted(ordering),
+              sortedState = Map(columnIndex -> asc),
+              offset = 0)
         }
       }
 
     def onPageSizeChange(value: String): Callback =
       t.modState(_.copy(rowsPerPage = value.toInt))
 
-    def render(P: Props, S: State): VdomElement =
-      <.div(
-        P.style.reactTableContainer,
-        ReactSearchBox(onTextChange = onTextChange(P) _, style = P.searchBoxStyle)
-          .when(P.enableSearch),
-        settingsBar((P, this, S)),
-        tableC((P, S, this)),
-        Pager(S.rowsPerPage, S.filteredData.length, S.offset, onNextClick, onPreviousClick)
-      )
-  }
-
-  def getHeaderDiv(config: ColumnConfig[T]): TagMod = {
-    config.width.fold(<.th())(width => <.th(^.width := width))
-  }
-
-  def arrowUp: TagMod =
-    TagMod(^.width := 0.px,
-           ^.height := 0.px,
-           ^.borderLeft := "5px solid transparent",
-           ^.borderRight := "5px solid transparent",
-           ^.borderBottom := "5px solid black")
-
-  def arrowDown: TagMod =
-    TagMod(^.width := 0.px,
-           ^.height := 0.px,
-           ^.borderLeft := "5px solid transparent",
-           ^.borderRight := "5px solid transparent",
-           ^.borderTop := "5px solid black")
-
-  def emptyClass: TagMod =
-    TagMod(^.padding := "1px")
-
-  val tableC = ScalaComponent
-    .builder[(Props, State, Backend)]("table")
-    .render { $ =>
-      val (props, state, b) = $.props
-
+    def render(props: Props, state: State): VdomElement = {
+      def settingsBar = {
+        var value = ""
+        var options: List[String] = Nil
+        val total = state.filteredData.length
+        if (total > props.rowsPerPage) {
+          value = state.rowsPerPage.toString
+          options = immutable.Range
+            .inclusive(props.rowsPerPage, total, 10 * (total / 100 + 1))
+            .:+(total)
+            .toList
+            .map(_.toString)
+        }
+        <.div(props.style.settingsBar)(<.div(<.strong("Total: " + state.filteredData.size)),
+          DefaultSelect(label = "Page Size: ",
+            options = options,
+            value = value,
+            onChange = onPageSizeChange))
+      }
       def renderHeader: TagMod =
         <.tr(
           props.style.tableHeader,
           props.configs.zipWithIndex.map {
             case (config, columnIndex) =>
               val cell = getHeaderDiv(config)
-//              config.sortBy.fold(cell(config.name.capitalize))(sortByFn =>
-                cell(
-                  ^.cursor := "pointer",
-                  ^.onClick --> b.sort(config.ordering, columnIndex),
-                  config.name.capitalize,
-                  props.style
-                    .sortIcon(state.sortedState.isDefinedAt(columnIndex) && state.sortedState(
-                      columnIndex) == asc)
-                    .when(state.sortedState.isDefinedAt(columnIndex))
-              )
-              //)
-          }.toTagMod
-        )
+              //              config.sortBy.fold(cell(config.name.capitalize))(sortByFn =>
+              cell(
+                ^.cursor := "pointer",
+                ^.onClick --> sort(config.ordering, columnIndex),
+                config.name.capitalize,
+                props.style
+                  .sortIcon(state.sortedState.isDefinedAt(columnIndex) && state.sortedState(
+                    columnIndex) == asc)
+                  .when(state.sortedState.isDefinedAt(columnIndex)))
+            //)
+          }.toTagMod)
 
       def renderRow(model: T): TagMod =
         <.tr(
@@ -238,10 +213,9 @@ case class ReactTable[T](data: Seq[T],
             .map(
               config =>
                 <.td(^.whiteSpace.nowrap.when(config.nowrap),
-                     ^.verticalAlign.middle,
-                     config.cellRenderer(model)))
-            .toTagMod
-        )
+                  ^.verticalAlign.middle,
+                  config.cellRenderer(model)))
+            .toTagMod)
 
       val rows = state.filteredData
         .slice(state.offset, state.offset + state.rowsPerPage)
@@ -251,37 +225,40 @@ case class ReactTable[T](data: Seq[T],
         }
         .toTagMod
 
-      <.div(props.style.table, <.table(<.thead(renderHeader()), <.tbody(rows)))
+      <.div(
+        props.style.reactTableContainer,
+        ReactSearchBox(onTextChange = onTextChange(props) _, style = props.searchBoxStyle)
+          .when(props.enableSearch),
+        settingsBar,
+        <.div(props.style.table, <.table(<.thead(renderHeader()), <.tbody(rows))),
+        Pager(state.rowsPerPage, state.filteredData.length, state.offset, onNextClick, onPreviousClick))
     }
-    .build
+  }
 
-  val settingsBar =
-    ScalaComponent
-      .builder[(Props, Backend, State)]("settingbar")
-      .render { $ =>
-        val (p, b, s)             = $.props
-        var value                 = ""
-        var options: List[String] = Nil
-        val total                 = s.filteredData.length
-        if (total > p.rowsPerPage) {
-          value = s.rowsPerPage.toString
-          options = immutable.Range
-            .inclusive(p.rowsPerPage, total, 10 * (total / 100 + 1))
-            .:+(total)
-            .toList
-            .map(_.toString)
-        }
-        <.div(p.style.settingsBar)(<.div(<.strong("Total: " + s.filteredData.size)),
-                                   DefaultSelect(label = "Page Size: ",
-                                                 options = options,
-                                                 value = value,
-                                                 onChange = b.onPageSizeChange))
-      }
-      .build
+  def getHeaderDiv(config: ColumnConfig[T]): TagMod = {
+    config.width.fold(<.th())(width => <.th(^.width := width))
+  }
+
+  def arrowUp: TagMod =
+    TagMod(^.width := 0.px,
+      ^.height := 0.px,
+      ^.borderLeft := "5px solid transparent",
+      ^.borderRight := "5px solid transparent",
+      ^.borderBottom := "5px solid black")
+
+  def arrowDown: TagMod =
+    TagMod(^.width := 0.px,
+      ^.height := 0.px,
+      ^.borderLeft := "5px solid transparent",
+      ^.borderRight := "5px solid transparent",
+      ^.borderTop := "5px solid black")
+
+  def emptyClass: TagMod =
+    TagMod(^.padding := "1px")
 
   val component = ScalaComponent
     .builder[Props]("ReactTable")
-    .initialStateFromProps(p => State(filterText = "", offset = 0, p.rowsPerPage, p.data, Map()))
+    .initialStateFromProps(props => State(filterText = "", offset = 0, props.rowsPerPage, props.data, Map()))
     .renderBackend[Backend]
     .componentWillReceiveProps(e =>
       Callback.when(e.currentProps.data != e.nextProps.data)(
@@ -289,11 +266,11 @@ case class ReactTable[T](data: Seq[T],
     .build
 
   case class Props(data: Seq[T],
-                   configs: List[ColumnConfig[T]],
-                   rowsPerPage: Int,
-                   style: Style,
-                   enableSearch: Boolean,
-                   searchBoxStyle: ReactSearchBox.Style)
+    configs: List[ColumnConfig[T]],
+    rowsPerPage: Int,
+    style: Style,
+    enableSearch: Boolean,
+    searchBoxStyle: ReactSearchBox.Style)
 
   def apply() = component(Props(data, configs, rowsPerPage, style, enableSearch, searchBoxStyle))
 }
